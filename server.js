b3b209b7b3d921e9a764b8d93f15fc43bc7e8d83
Leonardo Ugrešić <123001850+MisterRus01@users.noreply.github.com>
2025-04-08 13:55:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-res.send('Pozdrav sa Railway servera!');
-});
+app.get('/slike', (req, res) => {
+    const folderPath = path.join(__dirname, 'public', 'Portfolio_slike');
+    const files = fs.readdirSync(folderPath);
+    const images = files
+    .filter(file => file.endsWith('.jpg') || file.endsWith('.svg'))
+    .map((file, index) => ({
+    url: `/images/${file}`,
+    id: `slika${index + 1}`,
+    title: `Slika ${index + 1}`
+    }));
+    res.render('slike', { images });
+    });
+        
 app.listen(PORT, () => {
 console.log('Server pokrenut na portu ${PORT}');
 });
+
